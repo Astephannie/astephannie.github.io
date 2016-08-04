6a241a1eb53ecf1fdfe00754b94cf9e0b0374168
Annie Stephannie <astephannie@gmail.com>
2016-08-03 20:13:57 -0500

    js en sobre-mi

--- js/app.js
+++ js/app.js
@@ -6,3 +6,12 @@ $(document).ready(function() {
 		$("div[data-filter-value*=" + filter + "]").show();
 	})
 })	
+
+$(document).ready(function() {
+	$(".filter-about").click(function(e) {
+		e.preventDefault();
+		var filter = $(this).attr("data-filter");
+		$(".filter-value-about").hide();
+		$("article[data-filter-value*=" + filter + "]").show();
+	})
+})	
