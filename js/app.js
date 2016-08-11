5da270de4ff32e098f34e84c3b661c89dc3af2f3
astephannie <astephannie@gmail.com>
2016-08-11 21:08:32 +0000

    Assets y mi experiencia

--- js/app.js
+++ js/app.js
@@ -4,14 +4,26 @@ $(document).ready(function() {
 		var filter = $(this).attr("data-filter");
 		$(".filter-value").hide();
 		$("div[data-filter-value*=" + filter + "]").show();
-	})
-})	
-
-$(document).ready(function() {
+	});
+	
 	$(".filter-about").click(function(e) {
 		e.preventDefault();
 		var filter = $(this).attr("data-filter");
 		$(".filter-value-about").hide();
 		$("article[data-filter-value*=" + filter + "]").show();
-	})
+	});
+	
+	/* Smooth Scroll */
+	$('a[href*="#"]:not([href="#"])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      if (target.length) {
+	        $('html, body').animate({
+	          scrollTop: target.offset().top
+	        }, 1000);
+	        return false;
+	      }
+	    }
+	});
 })	
